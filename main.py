6523546d777b5bdaa61e76a5014b7b1e8be9cba2
hhhkmbv <ugfnkigdxx@gmail.com>
2026-08-26 22:05:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 from pyrogram import Client
 
-# المعلومات مثبتة هنا مباشرة حتى تشتغل بدون أي مشاكل
 API_ID = 35368782
 API_HASH = "72dc553687bd0437165b5c9bbaca4447"
 STRING_SESSION = os.getenv("STRING_SESSION")
 
-# إعداد عميل بايروگرام
+if not STRING_SESSION:
+    raise ValueError("STRING_SESSION is missing from GitHub Secrets.")
+
 app = Client(
     "moriarty_bot",
     api_id=API_ID,
@@ -14,6 +15,9 @@ app = Client(
     session_string=STRING_SESSION
 )
 
-if __name__ == "__main__":
-    print("Starting bot session...")
+def main():
+    print("Bot is starting...")
     app.run()
+
+if __name__ == "__main__":
+    main()
